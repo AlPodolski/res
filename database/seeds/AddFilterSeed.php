191c133c61adcbff81164254d9a5e573ee57ae7c
Работа <sanyapodolski@gmail.com>
2022-03-22 16:40:19 +0200

    7 настроить фильтр по времени встречи + 8 настроить фильтр по Место встречи + 9 настроить фильтр по Национальность + 10 настроить фильтр по Цвет волос + 11 настроить фильтр по Интимная стрижка

--- database/seeds/AddFilterSeed.php
+++ database/seeds/AddFilterSeed.php
@@ -16,10 +16,10 @@ class AddFilterSeed extends Seeder
 
         $filterUrlService = new \App\Services\FilterUrlService();
 
-        foreach ($dataList as $dataItem){
+        foreach ($dataList as $dataItem) {
 
             $data = [
-                'filter_url' => $filterUrlService->makeUrlForFilterTable($dataItem->value) ,
+                'filter_url' => $filterUrlService->makeUrlForFilterTable($dataItem->value),
                 'related_class' => \App\Models\PostMetro::class,
                 'related_param' => 'metros_id',
                 'search_param' => 'posts_id',
@@ -34,10 +34,10 @@ class AddFilterSeed extends Seeder
 
         $dataList = \App\Models\Rayon::get();
 
-        foreach ($dataList as $dataItem){
+        foreach ($dataList as $dataItem) {
 
             $data = [
-                'filter_url' => $filterUrlService->makeUrlForFilterTable($dataItem->value) ,
+                'filter_url' => $filterUrlService->makeUrlForFilterTable($dataItem->value),
                 'related_class' => \App\Models\PostRayon::class,
                 'related_param' => 'rayons_id',
                 'search_param' => 'posts_id',
@@ -50,5 +50,75 @@ class AddFilterSeed extends Seeder
 
         }
 
+
+        $dataList = \App\Models\Time::get();
+
+        foreach ($dataList as $dataItem) {
+
+            $data = [
+                'filter_url' => $filterUrlService->makeUrlForFilterTable($dataItem->value),
+                'related_class' => \App\Models\PostTime::class,
+                'related_param' => 'param_id',
+                'search_param' => 'posts_id',
+                'related_id' => $dataItem->id,
+                'parent_class' => \App\Models\Time::class,
+            ];
+
+            DB::table('filters')->insert($data);
+
+        }
+
+        $dataList = \App\Models\Place::get();
+
+        foreach ($dataList as $dataItem) {
+
+            $data = [
+                'filter_url' => $filterUrlService->makeUrlForFilterTable($dataItem->value),
+                'related_class' => \App\Models\PostPlace::class,
+                'related_param' => 'place_id',
+                'search_param' => 'post_id',
+                'related_id' => $dataItem->id,
+                'parent_class' => \App\Models\Place::class,
+            ];
+
+            DB::table('filters')->insert($data);
+
+        }
+
+        $dataList = \App\Models\National::get();
+
+        foreach ($dataList as $dataItem) {
+
+            $data = [
+                'filter_url' => $filterUrlService->makeUrlForFilterTable($dataItem->value),
+                'related_class' => \App\Models\PostNational::class,
+                'related_param' => 'nationals_id',
+                'search_param' => 'post_nationals_id',
+                'related_id' => $dataItem->id,
+                'parent_class' => \App\Models\National::class,
+            ];
+
+            DB::table('filters')->insert($data);
+
+        }
+
+        $dataList = \App\Models\IntimHair::get();
+
+        foreach ($dataList as $dataItem) {
+
+            $data = [
+                'filter_url' => $filterUrlService->makeUrlForFilterTable($dataItem->value),
+                'related_class' => \App\Models\PostIntimHair::class,
+                'related_param' => 'intim_hair_id',
+                'search_param' => 'posts_id',
+                'related_id' => $dataItem->id,
+                'parent_class' => \App\Models\IntimHair::class,
+            ];
+
+            DB::table('filters')->insert($data);
+
+        }
+
     }
+
 }
