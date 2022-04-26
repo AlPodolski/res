ddaf9f8fce05216d6ba8ab4badd38c977fdd11b2
Работа <sanyapodolski@gmail.com>
2022-04-26 14:56:01 +0300

    кеш тега яндекс

--- app/Repositories/YandexRepository.php
+++ app/Repositories/YandexRepository.php
@@ -8,6 +8,18 @@ class YandexRepository
 {
     public function getTag($cityId)
     {
-        return YandexTag::where(['city_id' => $cityId])->select('tag')->get()->first();
+
+        if ($value = \Cache::get('yandex_tag_'.$cityId)) return $value;
+
+        else{
+
+            $value = YandexTag::where(['city_id' => $cityId])->select('tag')->get()->first();
+
+            \Cache::set('yandex_tag_'.$cityId, $value);
+
+        }
+
+        return $value;
+
     }
 }
