9b3dcab19e58ec520110a06b3c975076a2c6278c
Your Name <you@example.com>
2022-05-30 12:54:31 +0300

    адаптив к сортировкам

--- resources/views/includes/limit_and_order.blade.php
+++ resources/views/includes/limit_and_order.blade.php
@@ -5,9 +5,8 @@
 
 <div class="sort-block d-flex">
     <div class="order-block">
-        Сортировать
         <select class="metro-select" name="limit" id="sort-select" onchange="setSort()">
-            <option @if($sort == 'default') selected @endif value="default">Выбрать</option>
+            <option @if($sort == 'default') selected @endif value="default">Сортировать</option>
             <option @if($sort == 'price_asc') selected @endif value="price_asc">От дешевых к дорогим</option>
             <option @if($sort == 'price_desc') selected @endif value="price_desc">От дорогих к дешевым</option>
         </select>
