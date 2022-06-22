b5feddccd781ce1b50418d2faba84f807532f125
Your Name <you@example.com>
2022-06-22 14:48:54 +0300

    вывод случайных постов вверху

--- app/Repositories/TopPostListRepository.php
+++ app/Repositories/TopPostListRepository.php
@@ -21,7 +21,7 @@ class TopPostListRepository
                 ->select($columns)
                 ->where(['city_id' => $cityId])
                 ->limit($limit)
-                ->orderBy('id', 'RAND')
+                ->orderByRaw('RAND()')
                 ->get();
 
         });
