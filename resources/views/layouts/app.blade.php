6d02a2f8d7aa573f4edd686bb7d51258e87583e6
Your Name <you@example.com>
2022-07-28 13:24:01 +0300

    раздел с новыми

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,7 +53,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css?v=9') }}" rel="stylesheet">
-    <link href="{{ asset('css/style.css?v=3') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css?v=4') }}" rel="stylesheet">
 </head>
 <body>
 <div id="app">
