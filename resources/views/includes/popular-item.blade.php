854114bf8a807a15f743f0e3bf9f2a75eb02b079
Your Name <you@example.com>
2023-03-14 14:32:45 +0200

    правки

--- resources/views/includes/popular-item.blade.php
+++ resources/views/includes/popular-item.blade.php
@@ -1,36 +1,39 @@
 @php
     /* @var $topListItem \App\Models\TopList */
 @endphp
-<div class="popular-item post-photo-item">
-    <a href="/post/{{ $topListItem->post->url }}">
-        <picture>
+@if(isset($topListItem->post->avatar->file))
+    <div class="popular-item post-photo-item">
+        <a href="/post/{{ $topListItem->post->url }}">
+            <picture>
 
-            <source srcset="/139-185/thumbs{{str_replace('.jpg', '.webp', $topListItem->post->avatar->file)}}"
-                    media="(max-width: 361px)" type="image/webp">
-            <source srcset="/139-185/thumbs{{$topListItem->post->avatar->file}}"
-                    media="(max-width: 361px)" type="image/jpeg">
+                <source srcset="/139-185/thumbs{{str_replace('.jpg', '.webp', $topListItem->post->avatar->file)}}"
+                        media="(max-width: 361px)" type="image/webp">
+                <source srcset="/139-185/thumbs{{$topListItem->post->avatar->file}}"
+                        media="(max-width: 361px)" type="image/jpeg">
 
-            <source srcset="/370-526/thumbs{{str_replace('.jpg', '.webp', $topListItem->post->avatar->file)}}"
-                    media="(max-width: 400px)" type="image/webp">
-            <source srcset="/370-526/thumbs{{$topListItem->post->avatar->file}}"
-                    media="(max-width: 400px)" type="image/jpeg">
+                <source srcset="/370-526/thumbs{{str_replace('.jpg', '.webp', $topListItem->post->avatar->file)}}"
+                        media="(max-width: 400px)" type="image/webp">
+                <source srcset="/370-526/thumbs{{$topListItem->post->avatar->file}}"
+                        media="(max-width: 400px)" type="image/jpeg">
 
-            <source srcset="/370-526/thumbs{{str_replace('.jpg', '.webp', $topListItem->post->avatar->file)}}"
-                    media="(max-width: 400px)" type="image/webp">
-            <source srcset="/370-526/thumbs{{$topListItem->post->avatar->file}}"
-                    media="(max-width: 400px)" type="image/jpeg">
+                <source srcset="/370-526/thumbs{{str_replace('.jpg', '.webp', $topListItem->post->avatar->file)}}"
+                        media="(max-width: 400px)" type="image/webp">
+                <source srcset="/370-526/thumbs{{$topListItem->post->avatar->file}}"
+                        media="(max-width: 400px)" type="image/jpeg">
 
-            <source srcset="/163-238/thumbs{{str_replace('.jpg', '.webp', $topListItem->post->avatar->file)}}" type="image/webp">
-            <source srcset="/163-238/thumbs{{$topListItem->post->avatar->file}}" type="image/jpeg">
+                <source srcset="/163-238/thumbs{{str_replace('.jpg', '.webp', $topListItem->post->avatar->file)}}"
+                        type="image/webp">
+                <source srcset="/163-238/thumbs{{$topListItem->post->avatar->file}}" type="image/jpeg">
 
-            <img width="163px" height="238px"
-                 title="Проститутка {{ $topListItem->post->name }}"
-                 @if(!isset($topList) or $topList->first() != $topListItem)
-                     loading="lazy"
-                 @endif
-                 class="popular-img "
-                 srcset="/163-238/thumbs{{$topListItem->post->avatar->file}}"
-                 alt="{{ $topListItem->post->name }}">
-        </picture>
-    </a>
-</div>
+                <img width="163px" height="238px"
+                     title="Проститутка {{ $topListItem->post->name }}"
+                     @if(!isset($topList) or $topList->first() != $topListItem)
+                         loading="lazy"
+                     @endif
+                     class="popular-img "
+                     srcset="/163-238/thumbs{{$topListItem->post->avatar->file}}"
+                     alt="{{ $topListItem->post->name }}">
+            </picture>
+        </a>
+    </div>
+@endif
