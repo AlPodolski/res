9b743312bc2d2acf6a63cb1a7500b1acc7ee9ea3
Precision <sanyapodolski@gmail.com>
2023-09-04 16:16:00 +0300

    склейка

---
+++ app/Models/Redirect.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Redirect extends Model
+{
+
+    public $timestamps = false;
+
+    public $fillable = ['from', 'to'];
+
+}
