280a686eb1fee95a7a3a5eb375a4ffc2eeef6eba
Dell <alpodolski11@gmail.com>
2024-04-08 12:33:08 +0300

    верстка меток

--- app/Widgets/views/labels.blade.php
+++ app/Widgets/views/labels.blade.php
@@ -4,8 +4,10 @@
 
 @if(!empty($labelList))
 
-    @foreach($labelList as $value)
-        <a class="filter-label" href="{{ $value['url'] }}">{{ $value['value'] }}</a>
-    @endforeach
+    <div class="filter-label-wrap">
+        @foreach($labelList as $value)
+            <a class="filter-label" href="{{ $value['url'] }}"><img src="/images/plus.svg" alt=""> {{ $value['value'] }}</a>
+        @endforeach
+    </div>
 
 @endif
