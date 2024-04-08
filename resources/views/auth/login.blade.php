2cb67909ee04e9da22390bd5bcdce5a6f4fa1306
Dell <alpodolski11@gmail.com>
2024-04-08 10:43:46 +0300

    регистрация авторизация

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,77 +1,68 @@
-@extends('layouts.app')
+@extends('layouts.new_public')
 @section('title', 'Войти')
 @section('des', 'Войти')
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
+    <div class="col-12">
+        <form method="POST" class="login-form" action="/login" >
+            <h1>Войти</h1>
+            @csrf
 
-                    <div class="card-body">
-                        <form method="POST" class="form" action="/login">
-                            <h1>Войти</h1>
-                            @csrf
+            <div class="form-group">
+                <label for="email"
+                       class=" col-form-label text-md-right">Почта</label>
 
-                            <div class="form-group">
-                                <label for="email"
-                                       class=" col-form-label text-md-right">Почта</label>
+                <input id="email" type="email" class="text-input request-call-input @error('email') is-invalid @enderror"
+                       name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                                <input id="email" type="email" class="form-control request-call-input @error('email') is-invalid @enderror"
-                                       name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                <span class="invalid-feedback" role="alert">
+                @error('email')
+                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
+                @enderror
+            </div>
 
-                            <div class="form-group ">
-                                <label for="password"
-                                       class=" col-form-label text-md-right">Пароль</label>
+            <div class="form-group ">
+                <label for="password"
+                       class=" col-form-label text-md-right">Пароль</label>
 
-                                <input id="password" type="password"
-                                       class="form-control request-call-input @error('password') is-invalid @enderror" name="password"
-                                       required autocomplete="current-password">
+                <input id="password" type="password"
+                       class="text-input request-call-input @error('password') is-invalid @enderror" name="password"
+                       required autocomplete="current-password">
 
-                                @error('password')
-                                <span class="invalid-feedback" role="alert">
+                @error('password')
+                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
+                @enderror
+            </div>
 
-                            <script defer src='https://www.google.com/recaptcha/api.js'></script>
+            <script defer src='https://www.google.com/recaptcha/api.js'></script>
 
-                            <div id="register_recapcha" class="g-recaptcha" data-sitekey="6Lffq2EkAAAAAK4PuAXJjhnE1NOP1uUjANyEUxe_"></div>
+            <div id="register_recapcha" class="g-recaptcha" data-sitekey="6Lffq2EkAAAAAK4PuAXJjhnE1NOP1uUjANyEUxe_"></div>
 
-                            <div class="form-group">
-                                <a href="/register" id="toLogin">Регистрация</a>
-                            </div>
+            <div class="form-group">
+                <a href="/register" id="toLogin" class="cabinet_link ">Регистрация</a>
+            </div>
 
-                            <div class="form-group ">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember"
-                                           id="remember" {{ old('remember') ? 'checked' : '' }}>
+            <div class="form-group ">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="remember"
+                           id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                    <label class="form-check-label" for="remember">
-                                        Запомнить меня
-                                    </label>
-                                </div>
-                            </div>
-
-                            <div class="form-group row mb-0">
-
-                                <button type="submit" class="get-more-post-btn">
-                                    Авторизация
-                                </button>
-
-                            </div>
-
-                        </form>
-                    </div>
+                    <label class="form-check-label" for="remember">
+                        Запомнить меня
+                    </label>
                 </div>
             </div>
-        </div>
+
+            <div class="form-group row mb-0">
+
+                <button type="submit" class="review-btn">
+                    Авторизация
+                </button>
+
+            </div>
+
+        </form>
     </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,83 +1,75 @@
-@extends('layouts.app')
+@extends('layouts.new_public')
 @section('title', 'Регистрация')
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-body">
-                        <form method="POST" class="form" action="/register">
-                            <h1>Регистрация</h1>
-                            @csrf
+    <div class="col-12">
+        <form method="POST" class="login-form" action="/register">
+            <h1>Регистрация</h1>
+            @csrf
 
-                            <div class="form-group">
-                                <label for="name" class="col-form-label text-md-right">Имя</label>
+            <div class="form-group">
+                <label for="name" class="col-form-label text-md-right">Имя</label>
 
-                                <input id="name" type="text"
-                                       class="form-control request-call-input @error('name') is-invalid @enderror"
-                                       name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                <input id="name" type="text"
+                       class="text-input request-call-input @error('name') is-invalid @enderror"
+                       name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
-                                @error('name')
-                                <span class="invalid-feedback" role="alert">
+                @error('name')
+                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
-
-                            <div class="form-group ">
-                                <label for="email" class="col-form-label text-md-right">Почта</label>
-
-                                <input id="email" type="email"
-                                       class="form-control request-call-input @error('email') is-invalid @enderror"
-                                       name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-
-                            <div class="form-group ">
-                                <label for="password" class="col-form-label text-md-right">Пароль</label>
-
-                                <input id="password" type="password"
-                                       class="form-control request-call-input @error('password') is-invalid @enderror"
-                                       name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-
-                            <div class="form-group">
-                                <label for="password-confirm" class="col-form-label text-md-right">Повторите
-                                    пароль</label>
-
-                                <input id="password-confirm" type="password" class="form-control request-call-input"
-                                       name="password_confirmation" required autocomplete="new-password">
-
-                            </div>
-
-                            <script defer src='https://www.google.com/recaptcha/api.js'></script>
-
-                            <div id="register_recapcha" class="g-recaptcha" data-sitekey="6Lffq2EkAAAAAK4PuAXJjhnE1NOP1uUjANyEUxe_"></div>
-
-                            <div class="form-group">
-                                <a href="/login" id="toLogin">Авторизация</a>
-                            </div>
-
-                            <div class="form-group row mb-0">
-                                <button type="submit" class="get-more-post-btn">
-                                    Регистрация
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
+                @enderror
             </div>
-        </div>
+
+            <div class="form-group ">
+                <label for="email" class="col-form-label text-md-right">Почта</label>
+
+                <input id="email" type="email"
+                       class="text-input request-call-input @error('email') is-invalid @enderror"
+                       name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                @error('email')
+                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                @enderror
+            </div>
+
+            <div class="form-group ">
+                <label for="password" class="col-form-label text-md-right">Пароль</label>
+
+                <input id="password" type="password"
+                       class="text-input request-call-input @error('password') is-invalid @enderror"
+                       name="password" required autocomplete="new-password">
+
+                @error('password')
+                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <label for="password-confirm" class="col-form-label text-md-right">Повторите
+                    пароль</label>
+
+                <input id="password-confirm" type="password" class="text-input request-call-input"
+                       name="password_confirmation" required autocomplete="new-password">
+
+            </div>
+
+            <script defer src='https://www.google.com/recaptcha/api.js'></script>
+
+            <div id="register_recapcha" class="g-recaptcha" data-sitekey="6Lffq2EkAAAAAK4PuAXJjhnE1NOP1uUjANyEUxe_"></div>
+
+            <div class="form-group">
+                <a href="/login" id="toLogin" class="cabinet_link">Авторизация</a>
+            </div>
+
+            <div class="form-group row mb-0">
+                <button type="submit" class="review-btn">
+                    Регистрация
+                </button>
+            </div>
+        </form>
     </div>
 @endsection
