332654be4c0c3bc3ecefe2ce83271d7c52344670
Dell <alpodolski11@gmail.com>
2024-05-13 15:13:13 +0300

    редиректы

--- app/Http/Middleware/RedirectMiddleware.php
+++ app/Http/Middleware/RedirectMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\City;
 use Cache;
 use Carbon\Carbon;
 use Closure;
@@ -42,6 +43,35 @@ class RedirectMiddleware
             $city = preg_replace('/[0-9]+/', '', $actualCity) ;
 
             $request->route()->setParameter('city', $city);
+
+        }
+
+        $actualCity = preg_replace("/[0-9]/", '', $actualCity);
+
+        $cityInfo = \Cache::get('city_info-' . $actualCity);
+
+        if (!$cityInfo) {
+
+            $cityInfo = City::where('url', $actualCity)
+                ->first();
+
+            \Cache::set('city_info-' . $actualCity , $cityInfo);
+
+        }
+
+        if ($cityInfo) {
+
+            $actualHost = $cityInfo->actual_city . '.' . $cityInfo->domain;
+
+            if ($actualHost != $_SERVER['HTTP_HOST']) {
+
+                $url = 'https://' . $actualHost . $_SERVER['REQUEST_URI'];
+
+                header('Location: ' . $url, true, 301);
+
+                exit();
+
+            }
 
         }
 
