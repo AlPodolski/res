3b02eeea6f1a0b66519928b29df5367da4401967
Dell <alpodolski11@gmail.com>
2024-05-15 09:52:45 +0300

    редирект

--- app/Http/Middleware/RedirectMiddleware.php
+++ app/Http/Middleware/RedirectMiddleware.php
@@ -16,36 +16,6 @@ class RedirectMiddleware
 
         $actualCity =  $request->route('city');
 
-        if ($actualCity){
-
-            $expire = Carbon::now()->addHours(24);
-
-            $redirect = Cache::remember('redirect_' . $actualCity, $expire, function () use ($actualCity) {
-
-                $redirect = Redirect::where('from', $actualCity)->first();
-
-                if ($redirect) return $redirect;
-
-                return false;
-
-            });
-
-            if ($redirect){
-
-                $url = 'https://'.$redirect->to.'.'.env('DOMAIN').$_SERVER['REQUEST_URI'];
-
-                header('Location: '.$url, true, 301);
-
-                exit();
-
-            }
-
-            $city = preg_replace('/[0-9]+/', '', $actualCity) ;
-
-            $request->route()->setParameter('city', $city);
-
-        }
-
         $actualCity = preg_replace("/[0-9]/", '', $actualCity);
 
         $cityInfo = \Cache::get('city_info-' . $actualCity);
